9683b348519d333adbfc557064db7b0538ffb15c
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2017-09-20 21:06:09 +0200

    Updated the image creation python script

--- CreateDemoFiles/create_demo_files.py
+++ CreateDemoFiles/create_demo_files.py
@@ -1,9 +1,10 @@
 #!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
 
 """
-Create an image with text
- - uses Pillow (pip install Pillow)
- - uses OpenCv (pip install opencv-python)
+Creates music video demo video files
+ - uses Pillow (pip install Pillow) to create frames with text and color
+ - uses OpenCv (pip install opencv-python) to push them into one video file
  """
 
 import random
@@ -13,84 +14,107 @@ import os
 
 
 def random_color():
-    """Creates an random color - https://stackoverflow.com/a/28999469/7827128"""
-
+    """Creates an random color"""
     return (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 
 
 def create_image(filename, size=(1280, 720), message="Text", bg_color=random_color(), number=-1, text_color="white"):
-    """Creates a image .png file with the name of the given string - """
+    """Creates a image .png file with the text"""
 
-    # load a font
-    font = ImageFont.truetype("FiraCode-Regular.ttf", 50)
-    font2 = ImageFont.truetype("FiraCode-Regular.ttf", 30)
-
-    # create an image
+    # create an image that we can draw on
     img = Image.new("RGB", size, bg_color)
-
-    # create a draw object
     draw = ImageDraw.Draw(img)
 
-    width_2, height_2 = draw.textsize(message, font=font)
+    # load font for the main text (message)
+    font_main_text = ImageFont.truetype("FiraCode-Regular.ttf", 50)
+
+    # get position so that the message is located in the middle of the image
+    width_2, height_2 = draw.textsize(message, font=font_main_text)
     position_text = ((size[0] - width_2) / 2, (size[1] - height_2) / 2)
-    draw.text(position_text, message, fill=text_color, font=font)
 
+    # draw the text on the image
+    draw.text(position_text, message, fill=text_color, font=font_main_text)
+
+    # if wanted (number!=-1) add a sub text to the image
     if number != -1:
-        width_2, height_2 = draw.textsize(">> Demo file #" + str(number), font=font2)
-        position_text = ((size[0] - width_2) / 2, (size[1] - height_2) / 2 + 100)
-        draw.text(position_text, ">> Demo file #" + str(number), fill=text_color, font=font2)
 
+        # load font for the sub text
+        font_sub_text = ImageFont.truetype("FiraCode-Regular.ttf", 30)
+
+        # determine position of the sub text in the middle but slightly down
+        width_2, height_2 = draw.textsize(">> Demo file #" + str(number), font=font_sub_text)
+        position_text = ((size[0] - width_2) / 2, (size[1] - height_2) / 2 + 100)
+
+        # draw the sub text on the image
+        draw.text(position_text, ">> Demo file #" + str(number), fill=text_color, font=font_sub_text)
+
+    # save draw object as a image
     draw = ImageDraw.Draw(img)
     img.save(filename)
 
 
-def create_video(images, filename):
+def create_video(images, filename, framerate=1.0, seconds=1):
     """Creates a video file out of images"""
 
     # Determine the width and height from the first image
     frame = cv2.imread(images[0])
-    #cv2.imshow('video', frame)
     height, width, channels = frame.shape
 
-    # Define the codec and create VideoWriter object
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Be sure to use lower case
-    out = cv2.VideoWriter(filename, fourcc, 1.0, (width, height))
+    # Define the codec and create VideoWriter object (mp4 not possible right now)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(filename, fourcc, framerate, (width, height))
 
+    # Add all images to the video
     for image in images:
-        for y in range(3):
+        # Added an option for longer than 1s videos
+        for y in range(seconds):
             image_path = image
             frame = cv2.imread(image_path)
+            out.write(frame)
 
-            out.write(frame)  # Write out frame to video
-
-            #cv2.imshow('video', frame)
-            if (cv2.waitKey(1) & 0xFF) == ord('q'):  # Hit `q` to exit
-                break
-
-    # Release everything if job is finished
+    # "Release everything if job is finished" -> finish export of video
     out.release()
     cv2.destroyAllWindows()
 
-    print("The output video is {}".format(filename))
+    # Let the user know about this
+    print("- Exported the video file \"{}\"".format(filename))
 
 
 if __name__ == '__main__':
+    """Main method: Create the demo music video files"""
 
+    # create a directory for the files to export
     if not os.path.exists("demo_files"):
         os.makedirs("demo_files")
 
+    # number of video files wanted
     number_of_files = 101
 
+    # Let the user know about this
+    print("Exporting {} video files:".format(number_of_files - 1))
+
+    # determine how many leading zeroes we need for every number
     number_of_zeroes = "%0" + str(len(str(number_of_files - 1))) + "d"
+
+    # create all files
     for x in range(number_of_files):
+
+        # format the current number with the right amount of leading zeroes
         x = number_of_zeroes % x
+
+        # these are the images we want to create
         images = [("demo_files/start", "Hi :)", x), ("demo_files/end", "Bye!", x)]
+
+        # create a random background color for all of the pictures
         bg_color = random_color()
 
+        # create the images
         for image in images:
             create_image(image[0] + ".png", (1280, 720), image[1], bg_color, image[2])
 
-        create_video([x[0] + ".png" for x in images], "demo_files/Example Artist #" + str(x) + " - Example Title #" + str(x) + ".avi")
+        # create the video out of the created images (each image has 3s view time)
+        create_video([x[0] + ".png" for x in images], "demo_files/Example Artist #" + str(x) + " - Example Title #" + str(x) + ".avi", 1.0, 3)
 
+        # remove the created images
         for image in images:
             os.remove(image[0] + ".png")
