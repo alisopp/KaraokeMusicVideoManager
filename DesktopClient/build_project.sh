12faaa9345dc543834e41cf84a1847b6f1b73d93
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2017-09-23 23:43:20 +0200

    Added script for automatic building
    FINALLLY!!!!!!!
    Build the jar without eclipse - only with maven

---
+++ DesktopClient/build_project.sh
@@ -0,0 +1,7 @@
+echo "Welcome to the automatic build of the MusicVideoManager :)"
+echo "---------------------------------------------------------------"
+echo "Building jars:"
+mvn clean install
+echo "-- created \"..\\MusicVideoManager-x.x.x.jar\""
+echo "-- created \"..\\MusicVideoManager-x.x.x-jar-with-dependencies.jar\""
+sleep 4
