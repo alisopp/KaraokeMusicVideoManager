645fd8950ff5f3870e9b6b4872dfa68b2b92bfad
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2017-09-24 15:48:47 +0200

    Small update for shell script

--- DesktopClient/build_project.sh
+++ DesktopClient/build_project.sh
@@ -2,5 +2,3 @@ echo "Welcome to the automatic build of the MusicVideoManager :)"
 echo "---------------------------------------------------------------"
 echo "Building jars:"
 mvn clean install
-echo "-- created \"..\\MusicVideoManager-x.x.x.jar\""
-echo "-- created \"..\\MusicVideoManager-x.x.x-jar-with-dependencies.jar\""
--- build_run_configuration.sh
+++ build_run_configuration.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # build_run_configuration.sh
 # - your run configuration
