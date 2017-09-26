8116bd682caebc834e77b84d8617ae6930a70c07
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2017-09-26 23:47:30 +0200

    Fixed css of party, added standalone form css and cleaned through this the form.php script

--- WebInterfaces/php/form.php
+++ WebInterfaces/php/form.php
@@ -14,9 +14,6 @@ $myarray = explode(",", $_REQUEST['index']);
 	<!-- Scale text on for example mobile devices: -->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!-- Link to CSS file: -->
-	<link rel="stylesheet" href="../css/styles_static.css">
-	<link rel="stylesheet" href="../css/styles_searchable.css">
-	<link rel="stylesheet" href="../css/styles_party.css">
 	<link rel="stylesheet" href="../css/php-form.css">
 	<!-- Link to JS file: -->
 	<script src="../js/w3.js"></script>
@@ -26,9 +23,7 @@ $myarray = explode(",", $_REQUEST['index']);
 <body>
 
 	<!-- Floating button with link to current playlist -->
-	<a href="#" onclick="history.back();" class="float">
-		<img style="padding-top:15px" width="30px" height="30px" src="data:image/svg+xml,%3Csvg%20fill%3D%22%23FFFFFF%22%20height%3D%2248%22%20viewBox%3D%220%200%2024%2024%22%20width%3D%2248%22%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%3E%0A%20%20%20%20%3Cpath%20d%3D%22M0%200h24v24H0z%22%20fill%3D%22none%22/%3E%0A%20%20%20%20%3Cpath%20d%3D%22M20%2011H7.83l5.59-5.59L12%204l-8%208%208%208%201.41-1.41L7.83%2013H20v-2z%22/%3E%0A%3C/svg%3E" />
-	</a>
+	<a href="#" onclick="history.back();" class="floating-button"></a>
 	<div class="label-container">
 		<div class="label-text">Back to the list</div>
 	</div>
