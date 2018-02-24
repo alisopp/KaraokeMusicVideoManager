b7ff371b7eaa1e43c74145c5174d9a2884dc404d
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2018-02-24 14:57:02 +0100

    Http requests added

--- DesktopClient/src/main/java/future/HttpPostJsonExample.java
+++ DesktopClient/src/main/java/future/HttpPostJsonExample.java
@@ -1,10 +1,16 @@
 package future;
 
 import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
 
+import javax.json.Json;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
@@ -14,7 +20,8 @@ public class HttpPostJsonExample {
 	public static void main(String[] args) {
 
 		try {
-			System.out.println(HttpPostJsonExample.makePostRequest("http://127.0.0.1/add/song", "{\"hallo\":\"hi\"}"));
+			System.out.println(
+					HttpPostJsonExample.makePostRequestJson("http://127.0.0.1/add/song", "{\"hallo\":\"hi\"}"));
 		} catch (org.apache.http.conn.HttpHostConnectException e) {
 			System.err.println("Could not build a connection to the server!");
 		} catch (ClientProtocolException e) {
@@ -23,9 +30,17 @@ public class HttpPostJsonExample {
 			f.printStackTrace();
 		}
 
+		try {
+			String a = IOUtils.toString(new URL("https://jsonplaceholder.typicode.com/posts/1"),
+					Charset.forName("UTF-8"));
+			System.out.println(a);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
-	public static int makePostRequest(final String URL_STRING, final String JSON_STRING)
+	public static int makePostRequestJson(final String URL_STRING, final String JSON_STRING)
 			throws ClientProtocolException, IOException {
 
 		// make HttpClient
@@ -42,5 +57,14 @@ public class HttpPostJsonExample {
 
 		// return server status code
 		return response2.getStatusLine().getStatusCode();
+	}
+
+	public static Json getJsonFromRequest(final String URL_STRING) {
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpGet request = new HttpGet(URL_STRING);
+		request.addHeader("accept", "application/json");
+		// HttpResponse response = client.execute(request);
+		// String json = response.getEntity().getContent().toString();
+		return null;
 	}
 }
